02d595d339b2092619bc467eea47b0a4f35721ca
Layal Youssef Safa Al Wanni <NISSOLELO03@GMAIL.COM>
2022-11-24 22:18:34 -0300

    pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
